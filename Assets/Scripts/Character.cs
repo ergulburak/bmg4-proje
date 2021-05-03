c748ed9d3c4aab08cf69bb7e9bab07705ac27e6b
ergulburak <burakcan41@gmail.com>
2021-05-04 00:12:46 +0300

    Ekonomi, Karakter çeşitliliği
    Coin sistemi eklendi.
    Değişik karakterler eklendi.
    Scroll karakter seçim ekranı eklendi.
    Ses sistemi ön hzaırlığı yapıldı.

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public int characterSequenceNumber;
+    public string characterName;
+    public Color sawColor;
+    public int price;
+    public bool isFree = false;
+
+    public bool IsUnlocked
+    {
+        get
+        {
+            return (isFree || PlayerPrefs.GetInt(characterName, 0) == 1);
+        }
+    }
+
+    void Awake()
+    {
+        characterName = characterName.ToUpper();
+    }
+
+    public bool Unlock()
+    {
+        if (IsUnlocked)
+            return true;
+
+        if (CoinManager.Instance.Coins >= price)
+        {
+            PlayerPrefs.SetInt(characterName, 1);
+            PlayerPrefs.Save();
+            CoinManager.Instance.RemoveCoins(price);
+
+            return true;
+        }
+
+        return false;
+    }
+}
